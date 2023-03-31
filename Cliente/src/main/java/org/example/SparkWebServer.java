4856baff935218f9b780aeae0f0d2111ae6e7511
unknown <andres.martinez-v@labinfo.is.escuelaing.edu.co>
2023-03-31 14:46:38 -0500

    Parcial2

--- Cliente/src/main/java/org/example/SparkWebServer.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import static spark.Spark.get;
-
-public class SparkWebServer {
-    public static void main(String[] args) {
-        get("/Collatz", (req, res) -> HttpConnectionExample.Collatz(num));
-    }
-}
