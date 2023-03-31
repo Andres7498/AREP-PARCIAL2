a7f2b20dd9771a3d036b4ac2d65bce68a9734b5c
unknown <andres.martinez-v@labinfo.is.escuelaing.edu.co>
2023-03-31 14:58:52 -0500

    Parcial2

--- Collatz/src/main/java/org/example/SparkWebServer.java
+++ Collatz/src/main/java/org/example/SparkWebServer.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import static org.example.HttpConnectionExample.answer;
 import static spark.Spark.*;
 
 public class SparkWebServer {
     public static void main(String[] args) {
         //llamado a la funcione de collatz
-        get("/Collatz", (req, res) -> Collatz(req.queryParams()));
+        get("/Collatz", (req, res) -> Collatz(req.queryParams(num)));
     }
 
     static int getPort() {
